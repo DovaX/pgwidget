40239f57fa452e9b3e987cdf8eff03c353d63b32
Dominik Vach <vach.dominik@gmail.com>
2022-02-08 18:50:01 +0100

    release 0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='pgwidget',
-    version='0.3.4',
+    version='0.3.5',
     author='DovaX',
     author_email='dovax.ai@gmail.com',
     description='Widgets for pygame',
